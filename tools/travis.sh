8a7fada2cfec66c0fcc1d4ed2c146d9b3aedb10a
Andrew Schwartzmeyer <andschwa@microsoft.com>
2016-08-17 13:14:16 -0700

    Only run XDG tests

--- tools/travis.sh
+++ tools/travis.sh
@@ -2,7 +2,7 @@ set -x
 ulimit -n 4096
 # Only build packages for branches, not pull requests
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    powershell -c "Import-Module ./build.psm1; Start-PSBootstrap -Package; Start-PSBuild -CrossGen; Start-PSPackage; Start-PSPester; Start-PSxUnit"
+    powershell -c "Import-Module ./build.psm1; Start-PSBootstrap -Package; Start-PSBuild -CrossGen; Start-PSPackage; Start-PSPester -Path test/powershell/Host; Start-PSxUnit"
 else
     powershell -c "Import-Module ./build.psm1; Start-PSBootstrap; Start-PSBuild -CrossGen; Start-PSPester; Start-PSxUnit"
 fi
